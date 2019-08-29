42c813f3eb4de5b425594af06182d671314df201
Minh Trung <fesockfam@gmail.com>
2019-08-29 11:10:05 +0700

    ketnoi1

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,12 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/xin-chao-ban/{ten}/{lop?}', function($ten, $lop='17CDTH'){
+	return 'Xin Chào bạn <b>'. $ten. '</b><br> Lớp: <b>'. $lop.'<b>';
+});
+Route::get('/xin-chao/{ho}/{ten}/{namsinh?}', function($ho,$ten, $namsinh='1999'){
+	return 'Xin Chào bạn <b>'. $ho.'</b> <b>'. $ten. '</b><br> Năm sinh: <b>'. $namsinh.'<b>';
+})
+-> where(['ho'=> '[A-Z][a-z]+' , 'ten'=> '[A-Z][a-z]+' , 'namsinh'=>'[0-9]+']);
